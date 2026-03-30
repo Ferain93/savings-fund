e44b6b4bf3070b192da332d3d288f46f2d5b98ae
Fernanda Trujillo <77870297+Ferain93@users.noreply.github.com>
2026-03-30 10:18:25 -0500

    SCRUM-10 setup Vue 3 + Vite + Pinia + Router + SCSS (#1)

---
+++ src/views/LoanRequestView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>LoanRequestView</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ src/views/ProfitsView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>ProfitsView</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
